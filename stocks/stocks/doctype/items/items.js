5ac70ef4e18dd56355868f9afb3ec59568dc44b5
Osama Shujaa Aldeen <osm540@hotmail.com>
2024-04-24 15:32:30 +0000

    duplicate purches recipt and renamed to supply items requests

---
+++ stocks/stocks/doctype/items/items.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Osama Shujaa Aldeen and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Items", {
+// 	refresh(frm) {
+
+// 	},
+// });
